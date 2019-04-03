852b8bd960aed10f72bbb545616a44f517e40be9
Tanmay <tanmay.chaturvedi@gmail.com>
2019-04-03 18:33:56 +0530

    last template update

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     #path('', views.index, name='index'),
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 from django.template import Template, Context
+from .models import Post
+from django.utils import timezone
 import datetime
 
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'polls/post_detail.html', {'post': post})
+
+def post_list(request):
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'polls/post_list.html', {'posts' : posts})
+
+
+#It's scheduled to ship on {{ date|date:"F j, Y" }}
 
 #def index(request):
     #return render(request, 'polls/tem.html')
 
-def post_list(request):
-    return render(request, 'polls/post_list.html', {})
 
-
-#It's scheduled to ship on {{ date|date:"F j, Y" }}
 '''d = datetime.datetime.now()
 t = Template("""<p>Dear {{ name }},</p>
 <p> Thanks for placing an order from {{ company }}. It's shipped at {{ date }}.</p>
