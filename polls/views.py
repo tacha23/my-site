428c46c55b1a25002554a9574bb72a51305a3722
Tanmay <tanmay.chaturvedi@gmail.com>
2019-04-03 19:05:40 +0530

    changes for integrating html

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from django.template import Template, Context
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
+from django.shortcuts import redirect
 import datetime
 
 
@@ -16,7 +18,32 @@ def post_list(request):
     posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'polls/post_list.html', {'posts' : posts})
 
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'polls/post_edit.html', {'form': form})
 
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'polls/post_edit.html', {'form': form})
 #It's scheduled to ship on {{ date|date:"F j, Y" }}
 
 #def index(request):
